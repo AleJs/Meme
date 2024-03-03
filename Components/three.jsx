62b58f63d76f0ce5b54af09127e256dfbb9739c0
alejs <alejscript@gmail.com>
2024-03-02 21:22:36 -0300

    rata

--- Components/three.jsx
+++ Components/three.jsx
@@ -7,29 +7,28 @@ import { useFBX } from "@react-three/drei";
 import threeFbxloaderOffical from "three-fbxloader-offical";
 // import  Modelo  from "./Scene";
 
-// function Scene() {
-//   const ref = useRef();
-//   useFrame((_, delta) => {
-//     ref.current.rotation.x += 1 * delta;
-//     ref.current.rotation.y += 0.5 * delta;
-//   });
-//   const fbx = useFBX("/rat.fbx");
-//   return <primitive object={fbx} ref={ref} />;
-// }
+function Scene() {
+  const ref = useRef();
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 1 * delta;
+    ref.current.rotation.y += 0.5 * delta;
+  });
+  const fbx = useFBX("/rat.fbx");
+  return <primitive object={fbx} ref={ref} />;
+}
 
 const ThreeScene = ({ children }) => {
   return (
-    // <Suspense fallback={null}>
-    //   <Canvas camera={{ position: [0, 10, 70] }}>
-    //     <ambientLight intensity={0.1} />
-    //     <directionalLight color="#913703" position={[0, 0, 5]} />
+    <Suspense fallback={null}>
+      <Canvas camera={{ position: [0, 10, 70] }}>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="#913703" position={[0, 0, 5]} />
 
-    //     <mesh position={[0, 0, 1]}>
-    //       <Scene />
-    //     </mesh>
-    //   </Canvas>
-    // </Suspense>
-    <h1>hola</h1>
+        <mesh position={[0, 0, 1]}>
+          <Scene />
+        </mesh>
+      </Canvas>
+    </Suspense>
   );
 };
 
